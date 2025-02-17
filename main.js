6bfda75dcd95599bf4f6887d40e8685a19adf3fd
TMMSoftware <eng.miguelaz+tmmsoftware@gmail.com>
2025-02-16 21:51:36 -0600

    add reset form behavior

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+// When the success message animation ends, reset the form.
+document.querySelectorAll('.success-message').forEach(successMessage => {
+  successMessage.addEventListener('animationend', function() {
+    // Find the closest form element and reset it.
+    const form = successMessage.closest('form');
+    form.reset();
+  });
+});
 
