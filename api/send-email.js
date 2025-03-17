e6b804f93e25bd79d485a8e15dd315cd777705e6
TMMSoftware <eng.miguelaz+tmmsoftware@gmail.com>
2025-03-17 14:32:52 -0700

    remove extra subject from resend.emails.send

--- api/send-email.js
+++ api/send-email.js
@@ -41,7 +41,6 @@ export default async function handler(req, res) {
     const data = await resend.emails.send({
       from: 'onboarding@resend.dev',
       to: email,
-      subject: 'Welcome to TMM Software!',
       subject: 'Thank you for joining TMM Software Early Access!',
       html: emailTemplate, // The loaded HTML file
     });
